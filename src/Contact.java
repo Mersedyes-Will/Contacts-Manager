bc24ac4a892a83f79cb6c0e6159b092a7d15125c
Will Roberts <willroberts2113@gmail.com>
2022-04-22 17:23:07 -0400

    some basic functionality

--- src/Contact.java
+++ src/Contact.java
@@ -1,28 +1,21 @@
 public class Contact {
 
-    //private int id;
     private String name;
     private String phoneNumber;
 
     public Contact(String name, String phoneNumber){
-
+        this.name = name;
+        this.phoneNumber = phoneNumber;
     }
 
-//    public int getId(int id){
-//        return id;
-//    }
-
     public String getName() {
+
         return name;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
-//    public void setId(int id) {
-//        this.id = id;
-//    }
 
     public void setName(String name) {
         this.name = name;
--- src/ContactsManagerRunner.java
+++ src/ContactsManagerRunner.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 public class ContactsManagerRunner {
@@ -7,30 +6,11 @@ public class ContactsManagerRunner {
     public static void main(String[] args) {
 
         HashMap<String, Contact> contacts = getContacts();
-        System.out.println(printContact(contacts));
-//        do{
-//            initGradesApp(contacts);
-//        } while(Input.yesNo("Would you like to search for another student? (y/n)"));
-//
-//    }
-//
-//    private static void initGradesApp(HashMap<String, Contact> contacts){
-//        System.out.println(contacts.keySet().stream().reduce("", (allNames, currentName) -> {
-//            return allNames + "\n -> " + currentName;
-//        }));
-//
-//        String username = Input.getString("\nPlease enter the GitHub username of a student if you want more info: \n");
-//
-//        if (contacts.containsKey(username)){
-//            Contact chosenOne = contacts.get(username);
-//            System.out.println("Student Name: " + chosenOne.getName().toUpperCase(Locale.ROOT));
-//            //System.out.println(chosenOne.getGradeAverage());
-//
-//        }else{
-//            System.out.println("No username found matching: " + username);
-//        }
-    }
 
+        printHeader();
+        printContact(contacts);
+
+    }
 
     private static HashMap<String, Contact> getContacts() {
         Contact contact1 = new Contact("Bob", "210-444-4444");
@@ -39,35 +19,46 @@ public class ContactsManagerRunner {
 
         Contact contact3 = new Contact("Joe", "719-890-1530");
 
-        HashMap<String, Contact> contacts = new HashMap<>();
+        Contact contact4 = new Contact("Mersedyes Livingston", "719-890-1530");
 
-//        String student1gh = "billiversustheworld";
+        HashMap<String, Contact> contacts = new HashMap<>();
 
         contacts.put("1", contact1);
         contacts.put("2", contact2);
         contacts.put("3", contact3);
+        contacts.put("4", contact4);
 
         return contacts;
     }
 
-    public static String stringFormatter(String contactProperty) {
-        for (int i = contactProperty.length(); i <= 15; i++) {
+    public static String nameFormatter(String contactProperty) {
+        for (int i = contactProperty.length(); i <= 24; i++) {
             contactProperty = contactProperty + " ";
         }
         return contactProperty;
     }
 
-    public void printHeader() {
-        System.out.println("      Name      |  Phone Number  ");
-        System.out.println("----------------------------------");
+    public static String phoneNumberFormatter(String contactProperty) {
+        for (int i = contactProperty.length(); i <= 12; i++) {
+            contactProperty = contactProperty + " ";
+        }
+        return contactProperty;
     }
 
-    public static void printContact(String contacts) {
-        for (Map.Entry<String, Object> person : contacts.entrySet()) {
-            System.out.println("key" + person.getKey() + "value" + person.getValue());
+    public static void printHeader() {
+        System.out.println("Name                     |  Phone Number  |");
+        System.out.println("-------------------------------------------");
+    }
+
+    public static void printContact(HashMap<String, Contact> contacts) {
+        for (Map.Entry<String, Contact> person : contacts.entrySet()) {
+
+            Contact chosenOne = person.getValue();
+
+            System.out.println(nameFormatter(chosenOne.getName()) + "|  " + phoneNumberFormatter(chosenOne.getPhoneNumber()) + " |");
+
         }
-//            Contact chosenOne = contacts.get(i);
-//            System.out.println(stringFormatter(chosenOne.getName()) + "|" + stringFormatter(chosenOne.getPhoneNumber()) + "|");
+
     }
 }
 
